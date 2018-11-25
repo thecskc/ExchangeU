ebd8ea7d54b8cdb5c1b3dd60f22460fd645a1ab2
Abdulkadir98 <ak98.narutokun@gmail.com>
2018-11-25 23:12:19 +0530

    removed google sign-in

---
+++ src/scripts/config.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyA6nNJCJkQVP1j8uZgx_0OgpIQ-5zEJGzY",
+    authDomain: "exchange-69a66.firebaseapp.com",
+    databaseURL: "https://exchange-69a66.firebaseio.com",
+    projectId: "exchange-69a66",
+    storageBucket: "exchange-69a66.appspot.com",
+    messagingSenderId: "51757558377"
+  };
+  firebase.initializeApp(config);
