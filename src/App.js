b3f824336bc2a89a455675e8ddcbc1436fe6a739
Abdul Kadir <ak98.narutokun@gmail.com>
2019-02-16 14:07:56 +0530

    Add Login form

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+import LoginForm from "./components/LoginForm";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <LoginForm />;
+  }
+}
+
+export default App;
