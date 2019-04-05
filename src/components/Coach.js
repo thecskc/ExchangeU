07a68af45e9ebb285f003c90376f054e1f49a06d
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2019-04-04 22:32:33 -0400

    added two cards per row but need to clean up

--- src/components/Coach.js
+++ src/components/Coach.js
@@ -14,12 +14,29 @@ class Coach extends Component {
     const coachesComponents = CoachesData.map(coach => (
       <CoachItem key={coach.id} {...coach} />
     ));
+
+    const coachRows = function(){
+        const retList = [];
+        for(let i=0;i<coachesComponents.length-1;i++){
+            retList.push(
+                <div style={{display:"block"}}>
+                    {coachesComponents[i]}
+                    {coachesComponents[i+1]}
+                </div>
+            );
+
+        }
+        return retList;
+
+    };
+
+    const coaches = coachRows();
     return (
       <div style={{ marginTop: 16 }}>
         <h1 style={{ textAlign: "center", marginBottom: 8 }}>
           Browse our Coaches
         </h1>
-        {coachesComponents}
+        {coaches}
       </div>
     );
   }
--- src/components/CoachItem.js
+++ src/components/CoachItem.js
@@ -1,70 +1,74 @@
 import React from "react";
-import { Link } from "@reach/router";
+import {Link} from "@reach/router";
 
 import "./CoachItem.css";
 
 function importAll(r) {
-  let images = {};
-  r.keys().map((item, index) => {
-    images[item.replace("./", "")] = r(item);
-  });
-  return images;
+    let images = {};
+    r.keys().map((item, index) => {
+        images[item.replace("./", "")] = r(item);
+    });
+    return images;
 }
 
 const images = importAll(
-  require.context("../assets/", false, /\.(png|jpe?g|svg)$/)
+    require.context("../assets/", false, /\.(png|jpe?g|svg)$/)
 );
 
 const fetaureTagStyles = {
-  position: "absolute",
-  top: 0,
-  right: 0,
-  marginRight: 12,
-  marginTop: 12,
-  background: "purple",
-  color: "white",
-  padding: 8
+    position: "absolute",
+    top: 0,
+    right: 0,
+    marginRight: 12,
+    marginTop: 12,
+    background: "purple",
+    color: "white",
+    padding: 8
 };
 
 export default class CoachItem extends React.Component {
-  constructor(props) {
-    super();
+    constructor(props) {
+        super();
 
-    this.state = {};
+        this.state = {};
 
-    this.handleClick = this.handleClick.bind(this);
-  }
+        this.handleClick = this.handleClick.bind(this);
+    }
 
-  handleClick() {}
+    handleClick() {
+    }
 
-  render() {
-    return (
-      <div className="card" style={{ position: "relative" }}>
-        <div style={fetaureTagStyles}>
-          <span>Featured ☆</span>
-        </div>
-        <img
-          src={images[`${this.props.img}.png`]}
-          style={{ margin: 16, height: 64, width: 64 }}
-          alt={this.props.img}
-        />
-        <div className="container">
-          <h4>
-            <b>{this.props.firstName}</b>
-          </h4>
-          <p style={{ marginTop: 16 }}>Software Engineer</p>
-          <p>{this.props.company}</p>
-          <p> {`Experience of ${this.props.experience} years`}</p>
-          <p style={{ marginBottom: 16 }}>{`Charges ${
-            this.props.ratePerHour
-          }$ per hour`}</p>
-          <Link to={`appointment-form/${this.props.id}`}>
-            <button className="button" onClick={this.handleClick}>
-              <span>Book now </span>
-            </button>
-          </Link>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="card" style={{position: "relative"}}>
+                <div style={fetaureTagStyles}>
+                    <span>Income Sharing Agreement ☆</span>
+                </div>
+                <img
+                    src={images[`${this.props.img}.png`]}
+                    style={{margin: 16, height: 64, width: 64}}
+                    alt={this.props.img}
+                />
+                <div className="container">
+                    <h3>
+                        <b>{this.props.firstName}</b>
+                    </h3>
+                    <p style={{marginTop: 16}}>Software Engineer</p>
+                    <h5>{this.props.company}</h5>
+                    <h5> {`${this.props.experience} year(s) of experience`}</h5>
+                    <h4 style={{marginBottom: 16}}>{`Charges ${
+                        this.props.ratePerHour
+                        }$ per hour`}</h4>
+                    <hr/>
+                    <p>{this.props.bio}</p>
+                    <hr/>
+                    <Link to={`appointment-form/${this.props.id}`}>
+                        <button className="button" onClick={this.handleClick}>
+                            <span>Book now </span>
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        );
+    }
 }
