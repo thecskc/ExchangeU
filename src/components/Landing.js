581ee6f4a55b8bd910080977be4cb2a42d5dcc3e
shwetakilledar <shwetakilledar25@gmail.com>
2019-04-16 00:46:38 -0400

    added carousel and fixed extra space to the right on landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -8,12 +8,90 @@ import paymentLogo from "../assets/payment.svg";
 import trainingLogo from "../assets/training.svg";
 import OneOnOneTraining from "../assets/oneonone.svg";
 import Particles from "react-particles-js";
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const styles = {
+  root: {
+
+  },
+  slide: {
+    justifyContent: "center",
+    background: "white",
+    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+    borderRadius: "20px",
+    height: "auto",
+    width: "45%",
+    margin:"auto",
+    padding: "5vh 2vw",
+    textAlign: "center",
+    fontFamily: "inherit",
+  }
+};
+
+const reviews = [
+  { name: "Mo", review: `Overall I found my Exchange experience very helpful and
+                          encouraging. My coach was excellent, he identified my strengths
+                          and weaknesses correctly. He also suggested hints during the
+                          interview which steered me in the right direction. The problems
+                          were also well chosen to determine my skill level. I would
+                          recommend him to others and I got a lot out of just one session.`},
+  { name: "Jev" , review: `I would give my coach a 4.75/5. He was very good at making sure
+                            each interview was scheduled according to my needs, and I felt
+                            much more confident in my interview. In the short amount of time
+                            we had, we were able to work through a good amount of problems,
+                            and he chose the ones that exercised my abilities for thinking
+                            problems through. I would use Exchange again, and I would
+                            definitely recommend it to a friend.`},
+  { name: "Hsiang" , review: ` My coach was very attentive to what I needed. I really needed
+                              some extra resources to prepare for my upcoming interviews, so I
+                              contacted Exchange for more info. The founders of Exchange
+                              connected me with a Google coach, which was pretty awesome
+                              because he prepared me more thoroughly than my friends, clubs,
+                              or interview prep books did. I felt very confident going into my
+                              phone interviews and I'm certain I moved onto the next round.
+                              For me, using Exchange was all about increasing my chances and
+                              Exchange helped alot with that.`},
+  { name: "Moon", review: `I read a lot of books such as CTCI but I was not able to get to
+                            the level needed to solve some hard Leetcode problems. I met
+                            with my coach over a month and I am now able to handle the hard
+                            Leetcode problems much better than I was able to before.`},
+  { name: "Daniel", review: `My 3 sessions with my technical interview coach Kim were great!
+                            We talked about Uber and his role there and how he prepared for
+                            his interview. He was able to understand that I lagged in topics
+                            like DP and Graphs. So we spent a lot of time working on those
+                            areas. Now, I feel confident about my onsite interview in two
+                            weeks! Daniel’s Exchange`}
+]
+
 
 class Landing extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+          index: 0
+        };
+    }
+
+    handleChangeIndex = index => {
+      this.setState({
+        index,
+      });
+    };
+
+    getReviews(){
+      let reviewJSX= [];
+      reviews.forEach((r)=>{
+        reviewJSX.push(
+        <div style={Object.assign({}, styles.slide)}>
+          <p>{r.review}</p>
+          <span>- {r.name}</span>
+        </div>)
+      })
+      return reviewJSX
     }
 
     render() {
@@ -408,70 +486,13 @@ class Landing extends Component {
                     <br/>
 
                     <div className="landing-testimonials-cards-section">
-                        <div className="landing-testimonial-card">
-                            <h3>Mo's Exchange</h3>
-                            <br/>
-
-                            <p>
-                                Overall I found my Exchange experience very helpful and
-                                encouraging. My coach was excellent, he identified my strengths
-                                and weaknesses correctly. He also suggested hints during the
-                                interview which steered me in the right direction. The problems
-                                were also well chosen to determine my skill level. I would
-                                recommend him to others and I got a lot out of just one session.
-                            </p>
-                        </div>
-                        <div className="landing-testimonial-card">
-                            <h3>Jev's Exchange</h3>
-                            <br/>
-                            <p>
-                                I would give my coach a 4.75/5. He was very good at making sure
-                                each interview was scheduled according to my needs, and I felt
-                                much more confident in my interview. In the short amount of time
-                                we had, we were able to work through a good amount of problems,
-                                and he chose the ones that exercised my abilities for thinking
-                                problems through. I would use Exchange again, and I would
-                                definitely recommend it to a friend.
-                            </p>
-                        </div>
-                        <div className="landing-testimonial-card">
-                            <h3>Hsiang's Exchange</h3>
-                            <br/>
-                            <p>
-                                My coach was very attentive to what I needed. I really needed
-                                some extra resources to prepare for my upcoming interviews, so I
-                                contacted Exchange for more info. The founders of Exchange
-                                connected me with a Google coach, which was pretty awesome
-                                because he prepared me more thoroughly than my friends, clubs,
-                                or interview prep books did. I felt very confident going into my
-                                phone interviews and I'm certain I moved onto the next round.
-                                For me, using Exchange was all about increasing my chances and
-                                Exchange helped alot with that.
-                            </p>
-                        </div>
-                        <div className="landing-testimonial-card">
-                            <h3>Moon's Exchange</h3>
-                            <br/>
-                            <br/>
-                            <p>
-                                I read a lot of books such as CTCI but I was not able to get to
-                                the level needed to solve some hard Leetcode problems. I met
-                                with my coach over a month and I am now able to handle the hard
-                                Leetcode problems much better than I was able to before.
-                            </p>
-                        </div>
-                        <div className="landing-testimonial-card">
-                            <h3>Daniel's Exchange</h3>
-                            <br/>
-                            <p>
-                                My 3 sessions with my technical interview coach Kim were great!
-                                We talked about Uber and his role there and how he prepared for
-                                his interview. He was able to understand that I lagged in topics
-                                like DP and Graphs. So we spent a lot of time working on those
-                                areas. Now, I feel confident about my onsite interview in two
-                                weeks! 
-                            </p>
-                        </div>
+                      <AutoPlaySwipeableViews
+                        index={this.state.index}
+                        onChangeIndex={this.handleChangeIndex}
+                        interval = {8000}
+                        >
+                        {this.getReviews()}
+                      </AutoPlaySwipeableViews>
                     </div>
                 </div>
             </div>
